d0f35406f9a8e412f40c180ac39f10bd89a94df7
Kevin Hartmann <kevinhartmann23@gmail.com>
2020-12-02 14:48:47 -0700

    Finished Game test and File

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,14 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = undefined;
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -12,6 +17,23 @@ class Game {
 
   printQuestion(round) {
       util.main(round);
+  }
+
+  start(cards){
+    let newCards = [];
+    cards.forEach( card => {
+      let newCard = new Card(
+        card.id,
+        card.question,
+        card.answers,
+        card.correctAnswer);
+      return newCards.push(newCard)
+    })
+    let deck = new Deck(newCards);
+    let round = new Round(deck);
+    this.currentRound = round;
+    this.printMessage(deck, round);
+    this.printQuestion(round);
   }
 }
 
